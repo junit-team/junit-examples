c6e5452103d3191d56a0008d9170f2923ec17c31
JUnit Team <team@junit.org>
2026-01-06 15:25:47 +0000

    Use 5.14.2

--- junit-modular-world/src/build/Project.java
+++ junit-modular-world/src/build/Project.java
@@ -43,9 +43,9 @@ record Project() {
     //
     // download main and test dependencies
     //
-    String platformVersion = "1.14.0";
-    String jupiterVersion = "5.14.0";
-    String vintageVersion = "5.14.0";
+    String platformVersion = "1.14.2";
+    String jupiterVersion = "5.14.2";
+    String vintageVersion = "5.14.2";
     get("lib", "org.junit.platform", "junit-platform-commons", platformVersion);
     get("lib", "org.junit.platform", "junit-platform-console", platformVersion);
     get("lib", "org.junit.platform", "junit-platform-engine", platformVersion);
