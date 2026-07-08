0d50d210a0b1aa4e33f203ac857cf3e9e8827f5f
M.P. Korstanje <rien.korstanje@gmail.com>
2026-07-08 14:42:07 +0200

    Use identical kotlin examples the Maven and Gradle starter projects

--- junit-jupiter-starter-maven-kotlin/src/test/kotlin/com/example/project/CalculatorTests.kt
+++ junit-jupiter-starter-maven-kotlin/src/test/kotlin/com/example/project/CalculatorTests.kt
@@ -13,25 +13,38 @@ package com.example.project
 import org.junit.jupiter.api.Assertions.assertEquals
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
-internal class CalculatorTests {
+class CalculatorTests {
 
-	@Test
-	internal fun `1 + 1 = 2`() = assertEquals(2, Calculator().add(1, 1), "1 + 1 should equal 2")
+    @Test
+    fun `1 + 1 = 2`() {
+        val calculator = Calculator()
+        assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2")
+    }
 
-	@ParameterizedTest(name = "{0} + {1} = {2}")
-	@CsvSource(
-			"0 ,   1,   1",
-			"1 ,   2,   3",
-			"49,  51, 100",
-			"1 , 100, 101"
-	)
-	internal fun add(first: Int, second: Int, expectedResult: Int) {
-		val calculator = Calculator()
-		assertEquals(expectedResult, calculator.add(first, second)) {
-			"$first + $second should equal $expectedResult"
-		}
-	}
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource(
+        "0,    1,   1",
+        "1,    2,   3",
+        "49,  51, 100",
+        "1,  100, 101"
+    )
+    fun add(first: Int, second: Int, expectedResult: Int) {
+        val calculator = Calculator()
+        assertEquals(expectedResult, calculator.add(first, second)) {
+            "$first + $second should equal $expectedResult"
+        }
+    }
+
+    @Test
+    fun divisionByZeroError() {
+        val calculator = Calculator()
+        val exception = assertThrows<AssertionError> {
+            calculator.div(1, 0)
+        }
+        assertEquals("Division by Zero", exception.message)
+    }
 }
